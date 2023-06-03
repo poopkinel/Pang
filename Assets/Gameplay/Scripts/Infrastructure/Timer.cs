86672154e86a5dc155561926fd7187dad6a6d3f7
Ophek <poopkinel@gmail.com>
2023-06-03 12:51:06 +0300

    Create Timer class

--- Assets/Gameplay/Scripts/Infrastructure/Timer.cs
+++ Assets/Gameplay/Scripts/Infrastructure/Timer.cs
@@ -4,14 +4,40 @@ using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
-    void Start()
+    [SerializeField]
+    private float _timerToSet;
+
+    [SerializeField]
+    private float _timeElapsed;
+
+    [SerializeField]
+    private bool _timerOn = false;
+
+    public void SetTime(float timeToSet)
     {
-        
+        _timerToSet = timeToSet;
     }
 
-    // Update is called once per frame
+    public void SetTimerOn(bool activate)
+    {
+        _timerOn = activate;
+    }
+
     void Update()
     {
-        
+        if (!_timerOn)
+        { 
+            return; 
+        }
+
+        if (_timeElapsed <= 0)
+        {
+            return;
+        }
+
+        _timeElapsed -= Time.deltaTime;
+
     }
+
+    public bool TimeUp => _timerToSet <= 0;
 }
